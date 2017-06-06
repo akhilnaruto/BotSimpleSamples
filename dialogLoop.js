0a543f92562a0008ad7edc4eea94c3f4cad20c4c
unknown <1019422@4Y5YKC2J.jda.corp.local>
2017-06-06 10:31:08 +0530

    loop using dialogs

--- dialogLoop.js
+++ dialogLoop.js
@@ -14,11 +14,30 @@ var bot = new builder.UniversalBot(connector,[
         session.beginDialog('q&aDialog');
     },
     function(session , results){
-        session.send('Thanks %(name)s.. you are %(age)s and located in %(state)s.' , results.response);
+        session.send('Thanks %(name)s. you are %(age)s and located in %(state)s.' , results.response);
     }
 ]);
 
 //Add Q&A dialog
+
+bot.dialog('q&aDialog',[
+    function(session,args){
+        session.dialogData.index = args ? args.index : 0;
+        session.dialogData.form = args ? args.form : {};
+
+        builder.Prompts.text(session,questions[session.dialogData.index].prompt);
+    },
+    function(session,results){
+        var field = questions[session.dialogData.index++].field;
+        session.dialogData.form[field] = results.response;
+
+        if(session.dialogData.index >= questions.length){
+            session.endDialogWithResult({response:session.dialogData.form});
+        }else{
+            session.replaceDialog('q&aDialog',session.dialogData);
+        }
+    }
+]);
 
 
 var questions = [
