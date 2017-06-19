1a1f88dace207f3a08ace387d0499086cf0c6497
Akhilesh Prathuri <Akhilesh.Prathuri@jda.com>
2017-06-19 12:29:03 +0530

    adding appid and password

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ server.listen(3978,function(){
     console.log('%s listening to %s', server.name , server.url);
 });
 
-var connector = new builder.ChatConnector();
+var connector = new  builder.ChatConnector({
+        appId :'13d2b7b4-b4aa-4382-a5fe-dae4339fdd5b',
+        appPassword : 'kx6naC1F3PwFakgPMAK4b4q',
+        gzipData: true
+    });
 
 server.post('/api/messages',connector.listen());
 
